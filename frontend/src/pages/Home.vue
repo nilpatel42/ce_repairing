41cf724c423a3b1b9d8066957ccffdee083e49c6
nilpatel42 <ndvadalia1@gmail.com>
2024-06-13 12:16:49 +0530

    Successfully Craeted UI

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,39 +1,122 @@
 <template>
-  <div class="max-w-3xl py-12 mx-auto">
-    <Button
-      icon-left="code"
-      @click="$resources.ping.fetch"
-      :loading="$resources.ping.loading"
-    >
-      Click to send 'ping' request
-    </Button>
-    <div>
-      {{ $resources.ping.data }}
+  <div class="mx-20">
+    <div class="flex flex-row items-center justify-between">
+      <h2 class="text-5xl font-black text-gray-900">Lists</h2>
+      <Button icon-left="plus">New List</Button>
     </div>
-    <pre>{{ $resources.ping }}</pre>
 
-    <Button @click="showDialog = true">Open Dialog</Button>
-    <Dialog title="Title" v-model="showDialog"> Dialog content </Dialog>
+    <div class="mt-2">
+      <Card title="General">
+        <div>
+          <!-- <ul>
+            <li
+              class="flex flex-row space-y-2 items-center justify-between"
+              v-for="action in actions.data"
+              :key="action.title"
+            >
+              <router-link :to="`/actions/${action.name}`">
+                {{ action.title }}
+              </router-link>
+              <Button @click="completeAction(action.name)" icon="check" />
+            </li>
+          </ul> -->
+
+          <Button @click="addActionDialogShown = true" icon-left="plus"
+            >New Action</Button
+          >
+        </div>
+      </Card>
+    </div>
+
+    <Dialog
+      :options="{
+        title: 'Add New Action',
+        actions: [
+          {
+            label: 'Add Action',
+            appearance: 'primary',
+            handler: ({ close }) => {
+              addAction()
+              close() // closes dialog
+            },
+          },
+          { label: 'Cancel' },
+        ],
+      }"
+      v-model="addActionDialogShown"
+    >
+      <template #body-content>
+        <div class="space-y-2">
+          <Input
+            v-model="action.title"
+            type="text"
+            required
+            placeholder="Give your action a title..."
+            label="Title"
+          />
+          <Input
+            v-model="action.category"
+            label="List"
+            type="select"
+            :options="categoryOptions"
+          />
+        </div>
+      </template>
+    </Dialog>
   </div>
 </template>
 
-<script>
-import { Dialog } from 'frappe-ui'
+<script setup>
+import { Dialog, createListResource, Card, Input } from 'frappe-ui'
+import { reactive, ref, computed } from 'vue'
 
-export default {
-  name: 'Home',
-  data() {
-    return {
-      showDialog: false,
-    }
+const action = reactive({
+  title: '',
+  category: 'General',
+})
+
+const addActionDialogShown = ref(false)
+
+const actions = createListResource({
+  doctype: 'Action',
+  fields: ['name', 'title', 'status', 'description', 'date', 'due_date'],
+  filters: {
+    status: 'ToDo',
   },
-  resources: {
-    ping: {
-      url: 'ping',
+  limit: 100,
+
+})
+actions.reload()
+
+const categories = createListResource({
+  doctype: 'Category',
+  fields: ['name'],
+  transform(categories) {
+    return categories.map((c) => c.name)
+  },
+  cache: 'actions',
+})
+categories.reload()
+
+const categoryOptions = computed(() => {
+  if (categories.list.loading || !categories.data) {
+    return []
+  }
+  return categories.data
+})
+
+const completeAction = (actionName) => {
+  actions.setValue.submit({
+    name: actionName,
+    status: 'Completed',
+    onSuccess() {
+      actions.reload()
     },
-  },
-  components: {
-    Dialog,
-  },
+  })
+}
+
+const addAction = () => {
+  console.log(action)
+  actions.insert.submit(action)
 }
 </script>
